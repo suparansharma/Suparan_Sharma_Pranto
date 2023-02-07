c48e9b39e9d8bfecd2f4049e650b09882a2d93f5
suparansharma <suparansharmap@gmail.com>
2023-02-07 23:47:11 +0600

    no update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -63,6 +63,202 @@ const Home = () => {
         </div>
         {/* <!--END HERO BACKGROUND *********************************************************************************--> */}
 
+        <main id="ts-content">
+
+        {/* <!--MY SERVICES ***********************************************************************************--> */}
+            <section id="my-services" className="ts-block ts-xs-text-center pb-0">
+                <div className="container">
+                    <div className="ts-title text-center">
+                        <h2>My Services</h2>
+                    </div>
+                    {/* <!--end ts-title--> */}
+                    <div className="row">
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-brushes.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>Web Design</h4>
+                                        <p className="mb-0">
+                                            Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                        {/* <!--end col-xl-4--> */}
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp" data-delay=".4s">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-lcd.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>Coding</h4>
+                                        <p className="mb-0">
+                                            Aenean pretium nulla libero, vitae iaculis libero efficitur a. Fusce ut augue finibus quam
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                        {/* <!--end col-xl-4--> */}
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp" data-delay=".5s">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-phone.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>App Developing</h4>
+                                        <p className="mb-0">
+                                            Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                       
+                        {/* <!--end col-xl-4--> */}
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp" data-delay=".3s">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-pencil.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>Copy Writing</h4>
+                                        <p className="mb-0">
+                                            Fusce lorem ex, fringilla eget consequat ut, molestie sit amet nibh. Nullam vitae tincidunt
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                        {/* <!--end col-xl-4--> */}
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp" data-delay=".1s">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-camera.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>Photography</h4>
+                                        <p className="mb-0">
+                                            Fusce lorem ex, fringilla eget consequat ut, molestie sit amet nibh. Nullam vitae tincidunt
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                       
+                        {/* <!--end col-xl-4--> */}
+                        <div className="col-sm-6 col-md-4 col-xl-4">
+                            <div className="ts-item" data-animate="ts-fadeInUp" data-delay=".2s">
+                                <div className="ts-item-content">
+                                    <div className="ts-item-header">
+                                        <figure className="icon">
+                                            <img src="assets/img/icon-video.png" alt=""/>
+                                        </figure>
+                                        {/* <!--end icon--> */}
+                                    </div>
+                                    {/* <!--end ts-item-header--> */}
+                                    <div className="ts-item-body">
+                                        <h4>Video Editing</h4>
+                                        <p className="mb-0">
+                                            Aenean pretium nulla libero, vitae iaculis libero efficitur a. Fusce ut augue finibus quam
+                                        </p>
+                                    </div>
+                                    {/* <!--end ts-item-body--> */}
+                                    <div className="ts-item-footer">
+                                        <a href="#" data-toggle="modal" data-target="#modal" className="ts-link-arrow-effect">
+                                            <span>Read More</span>
+                                        </a>
+                                    </div>
+                                    {/* <!--end ts-item-footer--> */}
+                                </div>
+                                {/* <!--end ts-item-content--> */}
+                            </div>
+                            {/* <!--end ts-item--> */}
+                        </div>
+                        {/* <!--end col-xl-4--> */}
+
+                    </div>
+                    {/* <!--end row--> */}
+                </div>
+                {/* <!--end container--> */}
+            </section>
+            {/* <!--END MY SERVICES *************************************************************************************--> */}
+
+            </main>
+
       </div>
 
   
